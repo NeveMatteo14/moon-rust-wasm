ecb5df10f02f99c6dc3015371813ea677d76f811
Matthew DellaNeve <mjdellaneve@gmail.com>
2024-02-28 15:26:43 -0500

    Added more styling. Attempting async run of interpret.

--- index.js
+++ index.js
@@ -7,24 +7,39 @@
 // eventually the manual initialization won't be required!
 import init, { interpret } from "./moonrust/pkg/moonrust.js";
 
+const submitButton = document.getElementById("submitButton");
+const codeInput = document.getElementById("codeInput");
+const interpreterOutput = document.getElementById("interpreterOutput");
+const codeOutput = document.getElementById("codeOutput");
+const loadingIcon = document.getElementById("loading");
+
+console.log(loadingIcon);
+
 async function run() {
-  // First up we need to actually load the wasm file, so we use the
-  // default export to inform it where the wasm file is located on the
-  // server, and then we wait on the returned promise to wait for the
-  // wasm to be loaded.
-  await init();
-
-  const submitButton = document.getElementById("submitButton");
-  const codeInput = document.getElementById("codeInput");
-
-  submitButton.addEventListener("click", (event) => {
-    try {
-      const result = interpret(codeInput.value);
-      console.log(result);
-    } catch (e) {
-      console.error(e);
+    async function runInterpreter(code) {
+        return interpret(code);
     }
-  });
+
+    // First up we need to actually load the wasm file, so we use the
+    // default export to inform it where the wasm file is located on the
+    // server, and then we wait on the returned promise to wait for the
+    // wasm to be loaded.
+    await init();
+
+    submitButton.addEventListener("click", async event => {
+        try {
+            loadingIcon.classList.remove("d-none");
+
+            const result = interpret(codeInput.value);
+            console.log(result);
+            codeOutput.textContent = result;
+            interpreterOutput.classList.remove("d-none");
+        } catch (e) {
+            console.error(e);
+        } finally {
+            loadingIcon.classList.add("d-none");
+        }
+    });
 }
 
 run();
