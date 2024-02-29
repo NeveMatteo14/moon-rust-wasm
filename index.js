58ec95d49ba4b43246bca69e728417307b450fbd
matthewdellaneve <mjdellaneve@gmail.com>
2024-02-28 23:21:58 -0500

    added index.css. Changed html element nesting and style.

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
-// Use ES module import syntax to import functionality from the module
-// that we have compiled.
-//
-// Note that the `default` import is an initialization function which
-// will "boot" the module and make it ready to use. Currently browsers
-// don't support natively imported WebAssembly as an ES module, but
-// eventually the manual initialization won't be required!
-// import init, { interpret } from "./moonrust/pkg/moonrust.js";
-
 const submitButton = document.getElementById("submitButton");
 const codeInput = document.getElementById("codeInput");
 const interpreterOutput = document.getElementById("interpreterOutput");
-const codeOutput = document.getElementById("codeOutput");
+const outputText = document.getElementById("outputText");
 const loadingIcon = document.getElementById("loading");
 
 let interpreter = new Worker("worker.js", { type: "module" });
-interpreter.onmessage = event => {
-    codeOutput.textContent = event.data;
-    interpreterOutput.classList.remove("d-none");
-    loadingIcon.classList.add("d-none");
+interpreter.onmessage = (event) => {
+  outputText.textContent = event.data;
+  interpreterOutput.classList.remove("d-none");
+  loadingIcon.classList.add("d-none");
 };
-interpreter.onerror = error => {
-    console.error(error);
-    loadingIcon.classList.add("d-none");
+interpreter.onerror = (error) => {
+  console.error(error);
+  loadingIcon.classList.add("d-none");
 };
 
-submitButton.addEventListener("click", event => {
-    loadingIcon.classList.remove("d-none");
-
-    // try {
-    //     const result = await interpret(codeInput.value);
-    //     codeOutput.textContent = result;
-    //     interpreterOutput.classList.remove("d-none");
-    // } catch (e) {
-    //     console.error(e);
-    // } finally {
-    //     loadingIcon.classList.add("d-none");
-    // }
-
-    interpreter.postMessage(codeInput.value);
+submitButton.addEventListener("click", (_) => {
+  loadingIcon.classList.remove("d-none");
+  interpreter.postMessage(codeInput.value);
 });
-
-// async function run() {
-//     // First up we need to actually load the wasm file, so we use the
-//     // default export to inform it where the wasm file is located on the
-//     // server, and then we wait on the returned promise to wait for the
-//     // wasm to be loaded.
-//     await init();
-
-// }
-
-// run();
--- worker.js
+++ worker.js
@@ -1,9 +1,24 @@
+// What: JS file for a worker thread that initializes the wasm files and
+// runs the interpreter when requested.
+//
+// Why: Calling the interpreter directly from the main thread will block
+// the browser until completion. For long running programs, this makes
+// the page unresponsive. By having a dedicated worker thread for the
+// interpreter, we can let users interact with the page while the Lua
+// program evaluates.
+
+// Imports the compiled MoonRust code. The `default` import is an
+// initialization function which will "boot" the module and make it ready
+// to use. Currently browsers don't support natively imported WebAssembly
+// as an ES module, but eventually the manual initialization won't be required!
 import init, { interpret } from "./moonrust/pkg/moonrust.js";
 
+// Load the wasm files
 async function run() {
-    await init();
+  await init();
 }
 
-onmessage = event => postMessage(interpret(event.data));
+// Call the interpreter when requested
+onmessage = (event) => postMessage(interpret(event.data));
 
 run();
